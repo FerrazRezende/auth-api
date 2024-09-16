30df7e37f86ed8d319d0ae0c0fadaee2c789f6bf
Matheus Ferraz <mtsferraz.rezende@gmail.com>
2024-09-16 14:55:17 -0300

    add staticfiles

--- server.py
+++ server.py
@@ -2,14 +2,19 @@
 Created by: Matheus Ferraz
 AuthAPI
 """
+import os
+from pathlib import Path
 
+from typing_extensions import Annotated
 
+from depends import get_current_user
 from router.person_router import person_router
 from router.session_router import session_router
 from router.auth_router import auth_router
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Dict
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, Depends, File, HTTPException
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 
 # Server instance
@@ -36,6 +41,49 @@ app.add_middleware(
 app.include_router(person_router, prefix="/person", tags=["Person"])
 app.include_router(session_router, prefix="/session", tags=["Session"])
 app.include_router(auth_router, prefix='/auth', tags=["Auth"])
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+
+UPLOAD_DIR = "static/img/"
+
+if not os.path.exists(UPLOAD_DIR):
+    os.makedirs(UPLOAD_DIR)
+
+def ensure_user_directory(username: str):
+    user_dir = os.path.join(UPLOAD_DIR, username)
+    Path(user_dir).mkdir(parents=True, exist_ok=True)
+    return user_dir
+
+
+def save_image(file: UploadFile, username: str) -> str:
+    user_dir = ensure_user_directory(username)
+
+    file_path = os.path.join(user_dir, "profilepic")
+
+    with open(file_path, "wb") as buffer:
+        buffer.write(file.file.read())
+
+    return file_path
+
+
+@app.post("/upload-photo/", tags=["Profile pic"])
+async def upload_user_photo(
+        file: Annotated[UploadFile, File(description="User profile image")],
+        current_user: dict = Depends(get_current_user)
+):
+    username = current_user.username
+
+    try:
+        file_path = save_image(file, username)
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error saving file")
+
+    return {"message": f"File uploaded successfully", "filepath": file_path}
+
 
 # Root route
 @app.get("/", tags=["Root"])
